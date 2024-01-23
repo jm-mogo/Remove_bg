3060810e82ef0445db11a421c03b538545d6bd94
mike <josemimore0@gmail.com>
2024-01-23 06:48:52 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+async function get_image() {
+    const response = await fetch("http://127.0.0.1:8800/remove_background");
+    const image = await response.blob();
+    document.getElementById("img-after").src = URL.createObjectURL(image);
+}
+
+async function uploadImg() {
+    const fileInput = document.querySelector("#filechooser");
+    const formData = new FormData();
+
+    formData.append("file", "human");
+    const options = {
+        method: "POST",
+        body: formData,
+    };
+    const response = await fetch("http://127.0.0.1:8800/upload_img", options);
+    document.getElementById("img-before").src = URL.createObjectURL(
+        fileInput.files[0]
+    );
+}
+
+// document.getElementById("upload").addEventListener("click", () => {
+//     console.log("upload");
+//     uploadImg();
+//     get_image();
+// });
+
+const fileInput = document.querySelector("#filechooser");
+fileInput.addEventListener("change", () => {
+    console.log("upload");
+    uploadImg();
+    get_image();
+});
+
+document.getElementById("set-model").addEventListener("change", (e) => {
+    const options = {
+        method: "post",
+        body: { model: "human" },
+        "Content-Type": "application/json",
+    };
+    fetch("http://127.0.0.1:8800/set_model", options);
+});
